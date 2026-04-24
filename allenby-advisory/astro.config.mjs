35d183e0aea852324e674108309cf668f36cdb27
Ade Allenby <ade@allenby-advisory.com>
2026-04-24 12:16:27 +0100

    SEO changes and redirects from old URLs

--- allenby-advisory/astro.config.mjs
+++ allenby-advisory/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://www.allenby-advisory.com',
+  integrations: [sitemap()],
 });
